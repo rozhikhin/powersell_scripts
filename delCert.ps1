8de3f2a1752678d71695225b844ee31eced66eac
Rozhikhin Alexander <rozhihin@gmail.com>
2019-03-27 17:46:03 +0300

    Add files via upload

---
+++ delCert.ps1
@@ -0,0 +1,6 @@
+﻿
+# Скрипт удаляет все сертификаьы из контейнера My (Личные), за исключением указанных с писке ListNoDeleteCert
+# Запускать с обычными правами, без повышения привилегий
+
+$ListNoDeleteCert = '47f5ffn53456789a459', '40f8f8f8f8f97ddfx1b23c565'
+ Get-ChildItem -Path  cert:CurrentUser\My | Where { $ListNoDeleteCert  -NotContains $_.SerialNumber } | Remove-Item
